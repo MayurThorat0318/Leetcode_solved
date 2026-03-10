3d796f46f68d212fc67b3ce811942595c33e2d04
Mayuresh Thorat <thoratmayuresh30@gmail.com>
2026-03-11 01:14:53 +0530

    Time: 2 ms (96.58%), Space: 48.4 MB (96.07%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] countBits(int n) {
+        int[] ans = new int[n + 1];
+
+        for (int i = 0; i <= n; i++) {
+            int num = i;
+            int count = 0;
+
+            while (num > 0) {
+                count += num & 1;   // check last bit
+                num = num >> 1;     // shift right
+            }
+
+            ans[i] = count;
+        }
+
+        return ans;
+    }
+}
